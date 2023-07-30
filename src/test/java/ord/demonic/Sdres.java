893e1a8b1687f6c7f158500db3efbf1138796fe4
princ <princ@Arthus>
2023-07-30 19:29:06 +0530

    manager first commit, project structure.

---
+++ src/test/java/ord/demonic/Sdres.java
@@ -0,0 +1,11 @@
+package ord.demonic;
+
+public class Sdres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi ra... typed by ravi");
+
+	}
+
+}
