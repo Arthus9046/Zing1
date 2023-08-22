946d47e1163a00b4970a261806e5d379bab21d02
princearthus9046@gmail.com <princearthus9046@gmail.com>
2023-08-22 13:03:04 +0530

    first file creation

---
+++ src/test/java/ord/demonic/Test1.java
@@ -0,0 +1,11 @@
+package ord.demonic;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hey welcome to command line interface enter");
+
+	}
+
+}
